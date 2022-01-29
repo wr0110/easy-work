ce1e62f0ddcbb4dd48cf8d319b08313d63616bbe
wr0110 <wittyraccoon0110@gmail.com>
2022-01-29 19:06:53 +0500

    feat: temp control brackets

---
+++ src/features/brackets/index.ts
@@ -0,0 +1,59 @@
+import {
+  Clock,
+  createEffect,
+  createEvent,
+  createStore,
+  sample,
+  Store,
+} from 'effector'
+
+type Result = {
+  valid: boolean
+  error?: string
+}
+
+export const createControlBrakets = (config: {
+  source: Store<string>
+  clock: Clock<unknown>
+  delay?: number
+}) => {
+  const $delay = createStore(config.delay ?? 1000)
+  const $stack = createStore<string[]>([])
+  const update = createEvent()
+  const $indexStack = createStore<number[]>([])
+
+  const debounce = createEffect<number, void>((delay) => {
+    return new Promise((res) => setTimeout(res, delay))
+  })
+
+  sample({
+    clock: update,
+    source: $delay,
+    target: debounce,
+  })
+
+  const updated = sample({
+    clock: debounce.done,
+    source: [config.source, $stack, $indexStack],
+  })
+
+  const validateBrackets = (
+    str: string,
+    stack: string[],
+    idxStack: string[]
+  ) => {
+    const brackets_rule = {
+      '(': ')',
+      '[': ']',
+      '{': '}',
+    }
+
+    for (const char of str) {
+      const last_brackets = stack[stack.length - 1]
+
+      if (char in brackets_rule) {
+        stack.push(char)
+      }
+    }
+  }
+}
