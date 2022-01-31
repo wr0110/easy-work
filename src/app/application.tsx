60881463a534d07fca4fb2714a39408a0b7a30a7
wr0110 <wittyraccoon0110@gmail.com>
2022-01-31 18:25:55 +0500

    feat(pages): add main page

--- src/app/application.tsx
+++ src/app/application.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Main } from '../pages'
 import { GlobalStyles } from './global-styles'
 
 export const Application = () => {
   return (
     <div>
       <GlobalStyles />
+      <Main />
     </div>
   )
 }
