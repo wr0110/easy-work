7d51eb341bf1dbc2fb8b1ee5fb6ebcab9046dd07
wr0110 <wittyraccoon0110@gmail.com>
2022-02-03 14:43:22 +0500

    feat: create 3 state for tasks

--- src/features/task-lifecycle/model.ts
+++ src/features/task-lifecycle/model.ts
@@ -12,3 +12,15 @@ export const takeTask = createEvent<{ taskID: string }>()
 export const resolveTask = createEvent<{ taskID: string }>()
 
 export const $taskLifecycle = createStore<TaskLifecycle[]>([])
+
+export const $idleTasks = $taskLifecycle.map((tasks) =>
+  tasks.filter((task) => task.status === 'idle')
+)
+
+export const $inProcessTasks = $taskLifecycle.map((tasks) =>
+  tasks.filter((task) => task.status === 'take')
+)
+
+export const $completedTasks = $taskLifecycle.map((tasks) =>
+  tasks.filter((task) => task.status === 'resolve')
+)
