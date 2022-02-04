5abc43826a3586a7a097ee90435b1884b77c5565
wr0110 <wittyraccoon0110@gmail.com>
2022-02-04 16:00:23 +0500

    chore: create colors variable

--- src/app/global-styles.ts
+++ src/app/global-styles.ts
@@ -1,6 +1,20 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
+  :root {
+    --gray100: hsl(220, 7%, 92%);
+    --gray200: hsl(216, 8%, 88%);
+    --gray300: hsl(210, 7%, 84%);
+
+    --yellow100: hsl(70, 100%, 86%);
+    --yellow200: hsl(70, 100%, 84%);
+    --yellow300: hsl(70, 100%, 80%);
+
+    --sienna100: hsl(23, 57%, 64%);
+    --sienna200: hsl(23, 57%, 60%);
+    --sienna300: hsl(23, 57%, 56%);
+  }
+
   html{
     font-size: 10px;
   }
