55ef53360637333821e3aa52e44d8cb0e4f3b7d3
wr0110 <wittyraccoon0110@gmail.com>
2022-02-13 17:05:13 +0500

    feat: add run logic

--- src/app/application.tsx
+++ src/app/application.tsx
@@ -1,11 +1,17 @@
 import { CssBaseline, GeistProvider } from '@geist-ui/core'
 import { useStore } from 'effector-react'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { $theme } from '../features/theme'
 import { Main } from '../pages'
+import { appStarted } from '../shared/config'
 
 export const Application = () => {
   const theme = useStore($theme)
+
+  useEffect(() => {
+    appStarted()
+  }, [])
+
   return (
     <>
       <GeistProvider themeType={theme}>
