635a2647e70cae7474d6e1541bb72e604df40f61
wr0110 <wittyraccoon0110@gmail.com>
2022-02-13 15:27:18 +0500

    refactor: remove styled-components

--- src/app/application.tsx
+++ src/app/application.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import { Main } from '../pages'
-import { GlobalStyles } from './global-styles'
 
 export const Application = () => {
   return (
     <>
-      <GlobalStyles />
       <Main />
     </>
   )
--- src/shared/ui/template/index.ts
+++
@@ -1,1 +0,0 @@
-export { ContentCentred } from './content-centred'
