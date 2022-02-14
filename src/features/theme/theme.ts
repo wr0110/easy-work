d4ada28332a9ab5f9dadfa87db309e5377e38a27
wr0110 <wittyraccoon0110@gmail.com>
2022-02-14 16:03:03 +0500

    feat: add config theme

---
+++ src/features/theme/theme.ts
@@ -0,0 +1,9 @@
+import { Themes } from '@geist-ui/core'
+
+export const kanbanLightTheme = Themes.createFromLight({
+  type: 'light',
+})
+
+export const kanbanDarkTheme = Themes.createFromDark({
+  type: 'dark',
+})
