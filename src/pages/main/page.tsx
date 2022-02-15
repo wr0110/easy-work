8c05d9343088553fb83ebc6b6450e744b08c3187
wr0110 <wittyraccoon0110@gmail.com>
2022-02-15 20:31:44 +0500

    feat: add container for projects

--- src/pages/main/page.tsx
+++ src/pages/main/page.tsx
@@ -1,3 +1,4 @@
+import { styled } from '@linaria/react'
 import React from 'react'
 import { ProjectPreview } from '../../entities/project/ui'
 import { BaseTemplate } from '../../shared/ui'
@@ -5,7 +6,16 @@ import { BaseTemplate } from '../../shared/ui'
 export const Main = () => {
   return (
     <BaseTemplate>
-      <ProjectPreview />
+      <Container>
+        {Array.from({ length: 4 }, (_, i) => (
+          <ProjectPreview key={i} />
+        ))}
+      </Container>
     </BaseTemplate>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
