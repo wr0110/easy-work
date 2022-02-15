327f80bc63b520e15ebd4d7007549fbc1d06da33
wr0110 <wittyraccoon0110@gmail.com>
2022-02-15 20:46:40 +0500

    feat: add link

--- src/entities/project/ui/organisms/project-preview.tsx
+++ src/entities/project/ui/organisms/project-preview.tsx
@@ -1,16 +1,22 @@
-import { Card, Text } from '@geist-ui/core'
+import { Card, Text, Image, Link } from '@geist-ui/core'
 import React, { FC } from 'react'
 import { Project } from '../..'
 
 export const ProjectPreview: FC<Partial<Project>> = () => {
   return (
-    <Card width="400px">
-      <Text h4 mb={0}>
-        hello world
-      </Text>
-      <Text type="secondary" small>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, odit!
-      </Text>
-    </Card>
+    <Link href="/">
+      <Card width="400px">
+        <Image
+          draggable={false}
+          src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.2.1449946290.1638748800"
+        />
+        <Text h4 mb={0}>
+          hello world
+        </Text>
+        <Text type="secondary" small>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, odit!
+        </Text>
+      </Card>
+    </Link>
   )
 }
