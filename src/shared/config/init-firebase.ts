ab8e93ce5b1cdb8b42ded425304cf0622d3e2375
wr0110 <wittyraccoon0110@gmail.com>
2022-02-16 20:06:15 +0500

    feat: add init firebase operator

---
+++ src/shared/config/init-firebase.ts
@@ -0,0 +1,46 @@
+import { attach, createEffect, createStore, sample, Unit } from 'effector'
+import { FirebaseApp, FirebaseOptions, initializeApp } from 'firebase/app'
+
+export const initFirebase = <T extends FirebaseOptions, R>(
+  config: T,
+  when: Unit<R>
+) => {
+  const $opened = createStore(false)
+  const $credentials = createStore(config)
+  const $firebaseApp = createStore<FirebaseApp | null>(null)
+
+  const initFirebaseFx = createEffect<FirebaseOptions, FirebaseApp, void>({
+    handler: async (credentials) => {
+      const app = initializeApp({
+        apiKey: credentials.apiKey,
+        authDomain: credentials.authDomain,
+        projectId: credentials.projectId,
+        storageBucket: credentials.storageBucket,
+        messagingSenderId: credentials.measurementId,
+        appId: credentials.appId,
+      })
+
+      return app
+    },
+  })
+
+  const startFirebaseApp = attach({
+    source: $credentials,
+    effect: initFirebaseFx,
+  })
+
+  sample({
+    source: when,
+    target: startFirebaseApp,
+  })
+
+  sample({
+    source: startFirebaseApp.doneData,
+    target: $firebaseApp,
+  })
+
+  return {
+    $firebaseApp,
+    $opened,
+  }
+}
