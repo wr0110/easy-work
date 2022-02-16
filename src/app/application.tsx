acc97a5d50b45c0cddc5d18905b21727d58722f5
wr0110 <wittyraccoon0110@gmail.com>
2022-02-16 19:59:36 +0500

    refactor: remove init firebase

--- src/app/application.tsx
+++ src/app/application.tsx
@@ -1,16 +1,9 @@
 import { CssBaseline, GeistProvider } from '@geist-ui/core'
-import { sample } from 'effector'
 import { useStore } from 'effector-react'
 import React, { useEffect } from 'react'
 import { $theme } from '../features/theme'
 import { Main } from '../pages'
 import { appStarted } from '../shared/config/run-logic'
-import { startFirebase } from './init-firebase'
-
-sample({
-  source: appStarted,
-  target: startFirebase,
-})
 
 export const Application = () => {
   const theme = useStore($theme)
