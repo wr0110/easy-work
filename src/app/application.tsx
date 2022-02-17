758debcb00d9e1f6906447e0fe03e803b78f3beb
wr0110 <wittyraccoon0110@gmail.com>
2022-02-17 15:06:27 +0500

    fix: fix page

--- src/app/application.tsx
+++ src/app/application.tsx
@@ -2,9 +2,9 @@ import { CssBaseline, GeistProvider } from '@geist-ui/core'
 import { useStore } from 'effector-react'
 import React, { useEffect } from 'react'
 import { $theme } from '../features/theme'
+import { Main } from '../pages'
 import { initFirebase } from '../shared/config/init-firebase'
 import { appStarted } from '../shared/config/run-logic'
-import { Main } from '../shared/pages'
 import { config } from './firebase-config'
 
 initFirebase({
