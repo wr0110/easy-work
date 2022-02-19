2d2931ff19b9d9369e71e30a16e1ebbf35d644b8
wr0110 <wittyraccoon0110@gmail.com>
2022-02-19 21:50:56 +0500

    chore: add params project router

--- src/pages/project/page.tsx
+++ src/pages/project/page.tsx
@@ -2,7 +2,7 @@ import { createRoute } from 'atomic-router'
 import React from 'react'
 import { BaseTemplate } from '../../shared/ui'
 
-export const projectRouter = createRoute()
+export const projectRouter = createRoute<{ id: string }>()
 
 export const Project = () => {
   return <BaseTemplate>hello</BaseTemplate>
