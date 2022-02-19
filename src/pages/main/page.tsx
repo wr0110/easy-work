8afcd3d352a7add78ca9f4488b6cc4eae323b9ce
wr0110 <wittyraccoon0110@gmail.com>
2022-02-19 16:43:56 +0500

    refactor: change event started and message

--- src/pages/main/page.tsx
+++ src/pages/main/page.tsx
@@ -1,21 +1,26 @@
 import { styled } from '@linaria/react'
-import React from 'react'
+import { createEvent } from 'effector'
+import React, { useEffect } from 'react'
 import { ProjectPreview } from '../../entities/project/ui'
-import { appStarted } from '../../shared/config/run-logic'
 import { showMessage } from '../../shared/lib/toast'
 import { BaseTemplate } from '../../shared/ui'
 import { Header } from '../../widgets/header'
 
+const pageMounted = createEvent()
+
 showMessage({
-  when: appStarted,
+  when: pageMounted,
   toast: () => ({
     type: 'success',
-    text: 'Welcome in main page',
+    text: 'Welcome to projects page',
     delay: 2000,
   }),
 })
 
 export const Main = () => {
+  useEffect(() => {
+    pageMounted()
+  }, [])
   return (
     <BaseTemplate header={<Header />}>
       <Container>
