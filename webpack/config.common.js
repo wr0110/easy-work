653687a217523c11b214153719d3edc35a4902fa
wr0110 <wittyraccoon0110@gmail.com>
2022-02-20 15:01:07 +0500

    chore: add common webpack config

---
+++ webpack/config.common.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const config = require('./constants')
+
+module.exports = {
+  context: config.context,
+  entry: config.entryMain,
+  resolve: {
+    extensions: config.extensions,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: config.indexHTML,
+      inject: 'body',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' },
+          {
+            loader: '@linaria/webpack-loader',
+          },
+        ],
+      },
+    ],
+  },
+}
