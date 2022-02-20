77f714bf5d0973e0991023f2d34a1760d403c61e
wr0110 <wittyraccoon0110@gmail.com>
2022-02-20 15:02:12 +0500

    chore: add entry webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,14 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const Dotenv = require('dotenv-webpack')
+require('dotenv').config()
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src/index.tsx'),
-  mode: 'development',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    hot: true,
-    port: 3000,
-    historyApiFallback: true,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.tsx', '.ts', '.js'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: './src/index.html', inject: 'body' }),
-    new MiniCssExtractPlugin({ filename: 'styles.css' }),
-    new Dotenv(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.ts(x)?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.tsx$/,
-        exclude: /node_modules/,
-        use: [
-          { loader: 'babel-loader' },
-          {
-            loader: '@linaria/webpack-loader',
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: process.env.NODE_ENV !== 'production',
-            },
-          },
-        ],
-      },
-    ],
-  },
-}
+const { merge } = require('webpack-merge')
+
+const common = require('./webpack/config.common')
+const dev = require('./webpack/config.dev')
+const prod = require('./webpack/config.prod')
+
+const isDev = process.env.NODE_ENV && process.env.NODE_ENV === 'development'
+
+const developmentConfig = merge([common, dev])
+const productionConfig = merge([common, prod])
+
+module.exports = isDev ? developmentConfig : productionConfig
