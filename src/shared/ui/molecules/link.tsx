a05f7295b46bce643bd1c26cad4045a9cee76b07
wr0110 <wittyraccoon0110@gmail.com>
2022-02-21 18:39:12 +0500

    feat: create custom link

---
+++ src/shared/ui/molecules/link.tsx
@@ -0,0 +1,62 @@
+import { css } from '@linaria/core'
+import { styled } from '@linaria/react'
+import { Link } from 'atomic-router-react'
+import React, { FC, ReactNode } from 'react'
+
+interface PropsBase {
+  className?: string
+  iconLeft?: ReactNode
+  iconRight?: ReactNode
+}
+
+type GetProps<T extends FC<never>> = Parameters<T>[0]
+
+type LinkProps = GetProps<typeof Link>
+
+export const CustomLink: FC<PropsBase & LinkProps> = ({
+  className,
+  iconLeft,
+  iconRight,
+  activeClassName,
+  children,
+  to,
+}) => {
+  return (
+    <Container className={className}>
+      <Link className={link} to={to} activeClassName={activeClassName}>
+        {iconLeft && (
+          <span data-element="icon" data-icon="left">
+            {iconLeft}
+          </span>
+        )}
+        {children}
+        {iconRight && (
+          <span data-element="icon" data-icon="right">
+            {iconRight}
+          </span>
+        )}
+      </Link>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  cursor: pointer;
+  padding: 0;
+`
+
+const link = css`
+  &[data-element='icon'] {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  & > [data-icon='left'] {
+    margin-right: 0.5rem;
+  }
+
+  & > [data-icon='right'] {
+    margin-left: 0.5rem;
+  }
+`
