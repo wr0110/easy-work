cd8737cf3408d867fc5b54b373735dcff5da2007
wr0110 <wittyraccoon0110@gmail.com>
2022-02-22 18:33:57 +0500

    feat: add load projects

--- src/entities/project/model.ts
+++ src/entities/project/model.ts
@@ -1,10 +1,11 @@
 import { createEvent, createStore } from 'effector'
+import { loadProjectsFx } from '~/shared/api/internal'
 
 export interface Project {
   projectID: string
   title: string
   description: string
-  isFinished: string
+  isFinished: boolean
   photoUrl: string
 }
 
@@ -14,3 +15,7 @@ export const addProject = createEvent<CreatedProject>()
 export const projectAdded = createEvent<Project>()
 
 export const $projects = createStore<Project[]>([])
+
+$projects.on(loadProjectsFx.doneData, (projects, answer) =>
+  projects.concat(answer)
+)
