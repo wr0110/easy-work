72fd59a36c821549e6f6d8bd4d70662d8c69a0ff
wr0110 <wittyraccoon0110@gmail.com>
2022-02-22 18:32:21 +0500

    refactor: add props

--- src/entities/project/ui/organisms/project-preview.tsx
+++ src/entities/project/ui/organisms/project-preview.tsx
@@ -4,19 +4,21 @@ import React, { FC } from 'react'
 import { paths } from '~/shared/lib/paths'
 import type { Project } from '../..'
 
-export const ProjectPreview: FC<Partial<Project>> = () => {
+export const ProjectPreview: FC<Project> = ({
+  projectID,
+  title,
+  description,
+  photoUrl,
+}) => {
   return (
-    <Link to={paths.project('2')}>
+    <Link to={paths.project(projectID)}>
       <Card width="400px">
-        <Image
-          draggable={false}
-          src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.2.1449946290.1638748800"
-        />
+        <Image src={photoUrl} />
         <Text h4 mb={0}>
-          hello world
+          {title}
         </Text>
         <Text type="secondary" small>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, odit!
+          {description}
         </Text>
       </Card>
     </Link>
