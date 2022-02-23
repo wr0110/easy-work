c7a0e5f85f28b3918d6e40c3f31bdcfddcacbfe7
wr0110 <wittyraccoon0110@gmail.com>
2022-02-23 14:57:40 +0500

    style: add box-shadow to header

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -6,25 +6,27 @@ import { UserCard } from '~/entities/user/ui'
 
 export const Header = () => {
   return (
-    <Grid.Container gap={3} height="60px" justify="center" alignItems="center">
-      <Grid xs={10} alignItems="center">
-        <ButtonMenu onClick={console.log}>
-          <ApplicationMenu />
-        </ButtonMenu>
-        <Text h2>Mirio</Text>
-      </Grid>
-      <Grid xs={3}>
-        <Input scale={1} iconRight={<Keyboard ctrl />} placeholder="Search" />
-      </Grid>
-      <Grid xs={2} alignItems="center">
-        <Button type="secondary" ghost auto scale={0.6}>
-          Create project
-        </Button>
-      </Grid>
-      <Grid xs={3} alignItems="center">
-        <UserCard fullname="robert kuzhin" />
-      </Grid>
-    </Grid.Container>
+    <HeaderContainer>
+      <Grid.Container height="100%" justify="center" alignItems="center">
+        <Grid xs={12} margin="0" height="100%">
+          <ButtonMenu onClick={console.log}>
+            <ApplicationMenu />
+          </ButtonMenu>
+          <Text h2>Mirio</Text>
+        </Grid>
+        <Grid xs={3}>
+          <Input scale={1} iconRight={<Keyboard ctrl />} placeholder="Search" />
+        </Grid>
+        <Grid xs={2}>
+          <Button type="secondary" ghost auto scale={0.6}>
+            Create project
+          </Button>
+        </Grid>
+        <Grid xs={3}>
+          <UserCard fullname="robert kuzhin" />
+        </Grid>
+      </Grid.Container>
+    </HeaderContainer>
   )
 }
 
@@ -35,4 +37,10 @@ const ButtonMenu = styled.button`
   padding: 0;
   outline: none;
   border: none;
+`
+
+const HeaderContainer = styled.header`
+  width: 100%;
+  box-shadow: 0 0 15px 0 rgb(0 0 0 / 10%);
+  height: 60px;
 `
