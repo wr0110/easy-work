b443e011a7bf9b7dc387f2581f4b88255c570f56
wr0110 <wittyraccoon0110@gmail.com>
2022-02-23 13:50:30 +0500

    refactor: refactor design header

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -1,23 +1,38 @@
+import { Button, Grid, Input, Keyboard, Text } from '@geist-ui/core'
+import { ApplicationMenu } from '@icon-park/react'
 import { styled } from '@linaria/react'
 import React from 'react'
 import { UserCard } from '~/entities/user/ui'
 
 export const Header = () => {
   return (
-    <Container>
-      <Title>Kanban</Title>
-      <UserCard fullname="robert kuzhin" />
-    </Container>
+    <Grid.Container gap={3} height="60px" justify="center" alignItems="center">
+      <Grid xs={10} alignItems="center">
+        <ButtonMenu onClick={console.log}>
+          <ApplicationMenu />
+        </ButtonMenu>
+        <Text h2>Mirio</Text>
+      </Grid>
+      <Grid xs={3}>
+        <Input scale={1} iconRight={<Keyboard ctrl />} placeholder="Search" />
+      </Grid>
+      <Grid xs={2} alignItems="center">
+        <Button type="secondary" ghost auto scale={0.6}>
+          Create project
+        </Button>
+      </Grid>
+      <Grid xs={3} alignItems="center">
+        <UserCard fullname="robert kuzhin" />
+      </Grid>
+    </Grid.Container>
   )
 }
 
-const Container = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  min-height: 4rem;
-`
-
-const Title = styled.div`
-  font-size: 1.5rem;
+const ButtonMenu = styled.button`
+  cursor: pointer;
+  margin-right: 1rem;
+  background: transparent;
+  padding: 0;
+  outline: none;
+  border: none;
 `
