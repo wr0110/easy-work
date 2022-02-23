9b4cbc3c397491c0255b5a6180ee38f60d6f36c0
wr0110 <wittyraccoon0110@gmail.com>
2022-02-23 16:24:27 +0500

    feat: add temp tabs

--- src/pages/workspace/page.tsx
+++ src/pages/workspace/page.tsx
@@ -1,4 +1,4 @@
-import { Grid, Loading, Text } from '@geist-ui/core'
+import { Grid, Loading, Tabs, Text } from '@geist-ui/core'
 import { useStore } from 'effector-react'
 import React from 'react'
 import { ProjectList } from '~/entities/project/ui'
@@ -15,8 +15,23 @@ export const Workspace = () => {
       <Grid xs={20} height="63px">
         <Text h3>Your works</Text>
       </Grid>
-      <Grid xs={20} height="63px">
+      <Grid xs={20}>
         {loading ? <Loading>Loading</Loading> : <ProjectList />}
+      </Grid>
+      <Grid xs={20} height="63px">
+        <Tabs initialValue="1" width="100%">
+          <Tabs.Item label="Finished" value="1">
+            HTTP is stateless, but not session less.
+          </Tabs.Item>
+          <Tabs.Item label="all" value="2">
+            Between the Web browser and the server, numerous computers and
+            machines relay the HTTP messages.
+          </Tabs.Item>
+          <Tabs.Item label="favorites" value="3">
+            Between the Web browser and the server, numerous computers and
+            machines relay the HTTP messages.
+          </Tabs.Item>
+        </Tabs>
       </Grid>
     </Grid.Container>
   )
