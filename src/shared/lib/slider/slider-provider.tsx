0ee47fd4315006e63b78e13c51e522fccb9c6c8e
wr0110 <wittyraccoon0110@gmail.com>
2022-02-27 19:17:01 +0500

    refactor: refactor type and classname

--- src/shared/lib/slider/slider-provider.tsx
+++ src/shared/lib/slider/slider-provider.tsx
@@ -1,32 +1,29 @@
+import { useKeenSlider } from 'keen-slider/react'
+import React, { FC, useLayoutEffect } from 'react'
 import {
-  KeenSliderHooks,
-  KeenSliderPlugin,
-  useKeenSlider,
-} from 'keen-slider/react'
-import React, { FC, ReactNode, useLayoutEffect } from 'react'
-import { addRef, slideChanged, sliderOpened } from './slider-effects'
+  addRef,
+  slideChanged,
+  sliderDestroyed,
+  sliderOpened,
+} from './slider-effects'
 
 type KeenParams = Parameters<typeof useKeenSlider>[0]
+
 interface Props {
-  children: ReactNode | ReactNode[]
   className?: string
-  plugins?: KeenSliderPlugin<unknown, unknown, KeenSliderHooks>[] | undefined
 }
 
-export const SliderProvide = ({
+export const SliderProvider: FC<Props & KeenParams> = ({
   children,
-  className,
-  plugins,
+  className = '',
   ...options
-}: Props & KeenParams) => {
-  const [sliderRef, sliderInstance] = useKeenSlider(
-    {
-      created: () => sliderOpened(),
-      slideChanged,
-      ...options,
-    },
-    plugins
-  )
+}) => {
+  const [sliderRef, sliderInstance] = useKeenSlider<HTMLDivElement>({
+    created: () => sliderOpened(),
+    destroyed: () => sliderDestroyed(),
+    slideChanged,
+    ...options,
+  })
 
   useLayoutEffect(() => {
     addRef(sliderInstance)
