5e393d96b6f3d3db36140c9808db661c43d282df
wr0110 <wittyraccoon0110@gmail.com>
2022-03-02 20:44:38 +0500

    feat(ui): create drag upload component

---
+++ src/shared/ui/molecules/drag-upload.tsx
@@ -0,0 +1,81 @@
+import { useTheme } from '@geist-ui/core'
+import { styled } from '@linaria/react'
+import React, { CSSProperties, FC } from 'react'
+import { DropzoneOptions, useDropzone } from 'react-dropzone'
+
+interface Props {
+  className?: string
+  accept?: DropzoneOptions['accept']
+  minSize?: DropzoneOptions['minSize']
+  maxSize?: DropzoneOptions['maxSize']
+  maxFiles?: DropzoneOptions['maxFiles']
+  multiple?: DropzoneOptions['multiple']
+  onDrop?: DropzoneOptions['onDrop']
+  validator?: DropzoneOptions['validator']
+}
+
+const DragUploadBase: FC<Props> = ({
+  accept,
+  className,
+  maxFiles,
+  maxSize,
+  minSize,
+  onDrop,
+  multiple,
+  validator,
+  children,
+}) => {
+  const theme = useTheme()
+
+  const { getRootProps, getInputProps, isDragAccept, isFocused } = useDropzone({
+    accept,
+    maxFiles,
+    maxSize,
+    minSize,
+    onDrop,
+    multiple,
+    validator,
+  })
+
+  const styles: CSSProperties = {
+    border: `1px dashed ${theme.palette.border}`,
+    borderRadius: theme.layout.radius,
+    backgroundColor: theme.palette.accents_1,
+    transition: 'border 0.2s ease 0s, color 0.2s ease 0s',
+  }
+
+  return (
+    <div className={className} {...getRootProps()}>
+      <DropArea
+        style={styles}
+        data-focus={isFocused}
+        data-accept={isDragAccept}
+      >
+        {children}
+      </DropArea>
+      <input data-element="file" {...getInputProps()} />
+    </div>
+  )
+}
+
+export const DragUpload = styled(DragUploadBase)`
+  height: 270px;
+  &[data-element='file'] {
+    display: none;
+  }
+`
+
+const DropArea = styled.div`
+  cursor: pointer;
+
+  height: 100%;
+  width: 100%;
+
+  &[data-accept='true'] {
+    border-color: #00e676 !important;
+  }
+
+  &[data-focus='true'] {
+    border-color: #666 !important;
+  }
+`
