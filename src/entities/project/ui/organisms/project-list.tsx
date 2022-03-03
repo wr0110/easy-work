d543d205264757d5186e7d6d7f6630a20a6a1543
wr0110 <wittyraccoon0110@gmail.com>
2022-03-03 18:05:51 +0500

    feat: add navigate events

--- src/entities/project/ui/organisms/project-list.tsx
+++ src/entities/project/ui/organisms/project-list.tsx
@@ -2,7 +2,7 @@ import { Loading } from '@geist-ui/core'
 import { styled } from '@linaria/react'
 import React, { FC } from 'react'
 import type { Project } from '~/shared/api/internal'
-import { PaperSlide, SliderProvider } from '~/shared/lib/slider'
+import { PaperSlide, slider, SliderProvider } from '~/shared/lib/slider'
 import { EmptyBlock } from '~/shared/ui'
 import { ProjectPreview } from './project-preview'
 
@@ -36,7 +36,12 @@ export const ProjectList: FC<Props> = ({ projects, loading }) => {
   }
 
   return (
-    <SliderProvider navigation slides={{ perView: 4 }}>
+    <SliderProvider
+      onNext={() => slider.next()}
+      onPrev={() => slider.prev()}
+      slides={{ perView: 4 }}
+      navigation
+    >
       {projects.map((project) => (
         <PaperSlideStyled key={project.projectID}>
           <ProjectPreview project={project} />
