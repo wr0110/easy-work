bc00bc9a85b23e2449efcdf271a176c7f65ca321
wr0110 <wittyraccoon0110@gmail.com>
2022-03-05 19:56:00 +0500

    feat: add choice theme

---
+++ src/features/theme/ui/organisms/theme-choice.tsx
@@ -0,0 +1,50 @@
+import { Select, useTheme } from '@geist-ui/core'
+import { Moon, Sun } from '@icon-park/react'
+import { styled } from '@linaria/react'
+import React from 'react'
+import { themeChoice } from '../../model'
+
+export const ThemeChoice = () => {
+  const theme = useTheme()
+
+  return (
+    <Select
+      style={{ minWidth: '93px', width: '90px' }}
+      scale={0.5}
+      h="28px"
+      pure
+      value={theme.type}
+      title="Choice theme"
+      onChange={themeChoice}
+    >
+      <Select.Option value="light">
+        <SelectContainer>
+          <Sun data-element="icon" size={14} />
+          <div>Light</div>
+        </SelectContainer>
+      </Select.Option>
+      <Select.Option value="dark">
+        <SelectContainer>
+          <Moon data-element="icon" size={14} />
+          <div>Dark</div>
+        </SelectContainer>
+      </Select.Option>
+    </Select>
+  )
+}
+
+const SelectContainer = styled.span`
+  width: 100%;
+  height: 18px;
+
+  width: auto;
+  height: 18px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  & > [data-element='icon'] {
+    margin: 6px 10px 0 2px;
+  }
+`
