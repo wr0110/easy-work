e5ec415819a7c54307dcb1384488a02a90d0f90b
wr0110 <wittyraccoon0110@gmail.com>
2022-03-05 16:38:00 +0500

    refactor: update design theme

--- src/features/theme/ui/organisms/switch-theme.tsx
+++ src/features/theme/ui/organisms/switch-theme.tsx
@@ -1,33 +1,56 @@
-import { reflect } from '@effector/reflect'
-import { Button, useTheme } from '@geist-ui/core'
-import { Moon, Sun } from '@icon-park/react'
-import React, { FormEventHandler } from 'react'
-import { $isDark, themeToggled } from '../..'
+import { Toggle, useTheme, Text } from '@geist-ui/core'
+import { Moon, Sun } from '@geist-ui/icons'
+import { styled } from '@linaria/react'
+import React from 'react'
 
 interface Props {
-  isDark?: boolean
-  onClick?: FormEventHandler<unknown>
+  toggleTheme(): void
 }
-
-export const SwitchThemeBase = ({ isDark, onClick }: Props) => {
+// @fix semantic use label with htmlFor
+export const SwitchTheme = ({ toggleTheme }: Props) => {
   const theme = useTheme()
+  const isDark = theme.type === 'dark'
   const icon = isDark ? <Moon /> : <Sun />
+
   return (
-    <Button
-      auto
-      type="abort"
-      icon={icon}
-      scale={0.8}
-      onClick={onClick}
-      color={theme.palette.foreground}
-    />
+    <Button onClick={() => toggleTheme()}>
+      <span data-element="icon">{icon}</span>
+      <Container>
+        <Text span>Dark Side</Text>
+        <Toggle scale={1.5} checked={isDark} />
+      </Container>
+    </Button>
   )
 }
 
-export const SwitchTheme = reflect({
-  view: SwitchThemeBase,
-  bind: {
-    isDark: $isDark,
-    onClick: () => themeToggled(),
-  },
-})
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+
+  max-width: 100%;
+  max-height: 100%;
+
+  width: 100%;
+  height: 2.8rem;
+
+  background-color: transparent;
+  border: none;
+
+  border-radius: 0.3rem;
+
+  & > [data-element='icon'] {
+    padding-right: 1.3rem;
+  }
+`
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  max-width: 100%;
+  max-height: 100%;
+
+  height: 100%;
+  width: 100%;
+`
