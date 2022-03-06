87162a19553bf487637eebd2d271c4a2edc02c70
wr0110 <wittyraccoon0110@gmail.com>
2022-03-06 15:45:08 +0500

    feat: add auth with provider

--- src/features/session/model.ts
+++ src/features/session/model.ts
@@ -1,11 +1,14 @@
-import { createEffect } from 'effector'
+import { attach, createEffect, createStore } from 'effector'
 import {
   AuthProvider,
   getAuth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
   signInWithPopup,
+  TwitterAuthProvider,
   UserCredential,
 } from 'firebase/auth'
-import { User } from '~/entities/user'
+import { $user, User } from '~/entities/user'
 
 export const baseAuthenticateFx = createEffect<
   { provider: AuthProvider },
@@ -23,3 +26,65 @@ export const baseAuthenticateFx = createEffect<
     }
   },
 })
+
+export const sessionDeleteFx = createEffect({
+  handler: async () => {
+    await getAuth().signOut
+  },
+})
+
+export const sessionGetFx = createEffect<never, User>({
+  handler: async () => {
+    const user = await getAuth().currentUser
+
+    if (user === null) {
+      throw 'user is not authorized'
+    }
+
+    return {
+      fullname: user.displayName || 'unknown',
+      photoUrl: user.photoURL || '',
+      email: user.email,
+    }
+  },
+})
+
+export const $sessionDeletePending = sessionDeleteFx.pending
+export const $isAuthenticated = $user.map((user) => user !== null)
+
+export const $providerGoogle = createStore(new GoogleAuthProvider())
+export const $providerGithub = createStore(new GithubAuthProvider())
+export const $providerTwitter = createStore(new TwitterAuthProvider())
+
+export const authWithGoogleFx = attach({
+  effect: baseAuthenticateFx,
+  source: $providerGoogle,
+  mapParams: (_, provider) => ({
+    provider,
+  }),
+})
+
+export const authWithGithub = attach({
+  effect: baseAuthenticateFx,
+  source: $providerGithub,
+  mapParams: (_, provider) => ({
+    provider,
+  }),
+})
+
+export const authWithTwitter = attach({
+  effect: baseAuthenticateFx,
+  source: $providerTwitter,
+  mapParams: (_, provider) => ({
+    provider,
+  }),
+})
+
+$user.on(
+  [
+    authWithGoogleFx.doneData,
+    authWithGithub.doneData,
+    authWithTwitter.doneData,
+  ],
+  (_, user) => user
+)
