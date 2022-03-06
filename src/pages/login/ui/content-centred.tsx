ffa2b1c38e6af5066af49405e9e5990ba7b1b89b
wr0110 <wittyraccoon0110@gmail.com>
2022-03-06 16:09:09 +0500

    feat: add content centred wrap

---
+++ src/pages/login/ui/content-centred.tsx
@@ -0,0 +1,10 @@
+import { styled } from '@linaria/react'
+
+export const ContentCentred = styled.section`
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+
+  margin-top: 10rem;
+`
