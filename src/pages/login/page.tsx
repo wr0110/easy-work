ae211fd130bdc095ffe96d11fc3c110a5e81923f
wr0110 <wittyraccoon0110@gmail.com>
2022-03-06 16:10:32 +0500

    refactor: decomposed components

--- src/pages/login/page.tsx
+++ src/pages/login/page.tsx
@@ -1,38 +1,21 @@
-import { Text, useTheme, Link, Button, Spacer, Divider } from '@geist-ui/core'
+import { Text, Link, Spacer, Divider } from '@geist-ui/core'
 import { Github, Globe, Twitter } from '@geist-ui/icons'
 import { styled } from '@linaria/react'
 import React from 'react'
 import { SubmittedProvider } from '~/features/session/ui'
-import { ThemeChoice } from '~/features/theme/ui'
 import {
   githubAuthClicked,
   googleAuthClicked,
   twitterAuthClicked,
 } from './model'
+import { ContentCentred, Header } from './ui'
 
 export const Login = () => {
-  const theme = useTheme()
   return (
     <>
-      <Header data-theme={theme.type}>
-        <Container>
-          <Text h2>Mirio</Text>
-          <div>
-            <Link
-              mr={1.2}
-              rel="noopener"
-              target="_blank"
-              href="https://github.com/lordSzn"
-            >
-              Contact
-            </Link>
-            <Link mr={1.2}>Sign Up</Link>
-            <ThemeChoice />
-          </div>
-        </Container>
-      </Header>
-      <Content>
-        <LoginForm>
+      <Header />
+      <ContentCentred>
+        <Form>
           <Text h1>Log in to Mirio</Text>
           <SubmittedProvider
             label="Continue with google"
@@ -57,45 +40,13 @@ export const Login = () => {
           <Link color underline>
             Continue with email
           </Link>
-        </LoginForm>
-      </Content>
+        </Form>
+      </ContentCentred>
     </>
   )
 }
 
-const Header = styled.header`
-  width: 100%;
-  height: 4rem;
-
-  &[data-theme='light'] {
-    box-shadow: inset 0 -1px 0 0 rgba(0, 0, 0, 0.1);
-  }
-
-  &[data-theme='dark'] {
-    box-shadow: 0 0 0 1px #333;
-  }
-`
-
-const Container = styled.div`
-  max-width: 1048px;
-  margin: 0 auto;
-  width: 100%;
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`
-
-const Content = styled.section`
-  height: 100vh;
-
-  display: flex;
-  justify-content: center;
-
-  margin-top: 10rem;
-`
-
-const LoginForm = styled.form`
+const Form = styled.form`
   max-width: 60rem;
 
   text-align: center;
