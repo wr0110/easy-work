1a17a2284848a48ce68e63c8fc2d4d239f8fe6d2
wr0110 <wittyraccoon0110@gmail.com>
2022-03-08 20:23:46 +0500

    fix: fix event logout

--- src/entities/session/ui/atoms/logout.tsx
+++ src/entities/session/ui/atoms/logout.tsx
@@ -2,13 +2,13 @@ import { LogOut } from '@geist-ui/icons'
 import { createEvent, sample } from 'effector'
 import React from 'react'
 import { PopoverAction } from '~/shared/ui'
-import { sessionDeleteFx } from '../../model'
+import { logout } from '../../model'
 
 export const logoutClicked = createEvent()
 
 sample({
   clock: logoutClicked,
-  target: sessionDeleteFx,
+  target: logout,
 })
 
 export const Logout = () => {
