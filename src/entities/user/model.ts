44bdf12a762ca1b209d563949c487b721a64ee1d
wr0110 <wittyraccoon0110@gmail.com>
2022-03-08 15:43:11 +0500

    feat: add event for update user

--- src/entities/user/model.ts
+++ src/entities/user/model.ts
@@ -1,4 +1,9 @@
-import { createStore } from 'effector'
+import { createEvent, createStore } from 'effector'
 import { User } from '~/shared/api/requests'
 
-export const $user = createStore<User | null>(null)
+export const updateUser = createEvent<User>()
+
+export const $user = createStore<User | null>(null).on(
+  updateUser,
+  (_, user) => user
+)
