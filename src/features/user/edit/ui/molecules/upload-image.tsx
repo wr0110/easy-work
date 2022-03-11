1041ae4103f439cf0290207124fd6967adabd841
wr0110 <wittyraccoon0110@gmail.com>
2022-03-11 18:17:28 +0500

    feat: add src for image

--- src/features/user/edit/ui/molecules/upload-image.tsx
+++ src/features/user/edit/ui/molecules/upload-image.tsx
@@ -1,17 +1,22 @@
 import { Avatar, Tooltip } from '@geist-ui/core'
-import React from 'react'
+import React, { FC } from 'react'
 import { useDropzone } from 'react-dropzone'
 
-export const UserUploadImage = () => {
+interface Props {
+  src?: string
+}
+
+export const UserUploadImage: FC<Props> = ({ src }) => {
   const { getRootProps, getInputProps } = useDropzone({
     accept: 'image/*',
     maxFiles: 1,
   })
+
   return (
     <div>
       <Tooltip type="dark" text="Click to upload photo" placement="bottom" trigger="hover">
         <label {...getRootProps()}>
-          <Avatar scale={7.6} />
+          <Avatar src={src} scale={7.6} />
         </label>
         <input {...getInputProps()} />
       </Tooltip>
