77fea440ba054fc0ae119944ff5bd816b7152d66
wr0110 <wittyraccoon0110@gmail.com>
2022-03-13 20:05:03 +0500

    feat: add disable button

--- src/entities/project/ui/organisms/project-creation.tsx
+++ src/entities/project/ui/organisms/project-creation.tsx
@@ -53,7 +53,13 @@ export const ProjectCreationForm: FC<Props> = ({ visible, close }) => {
           <Button width="70px" mr={0.4} onClick={() => close()}>
             cancel
           </Button>
-          <Button type="success" loading={loading} width="70px" onClick={() => formSubmitted()}>
+          <Button
+            type="success"
+            loading={loading}
+            disabled={loading}
+            width="70px"
+            onClick={() => formSubmitted()}
+          >
             submit
           </Button>
         </ButtonGroup>
