24c90b7564552e8a406af2d1e80117dbe9ab4f92
wr0110 <wittyraccoon0110@gmail.com>
2022-03-17 12:31:06 +0500

    feat: add favorite button

---
+++ src/features/favorite/ui/atoms/favorite-button.tsx
@@ -0,0 +1,20 @@
+import { HeartFill, Heart } from '@geist-ui/icons'
+import { styled } from '@linaria/react'
+import React, { FC } from 'react'
+
+interface Props {
+  isFavorite: boolean
+  className?: string
+  size?: number
+}
+
+export const FavoriteButton: FC<Props> = ({ isFavorite, className, size }) => {
+  const label = isFavorite ? <HeartFill size={size} color="red" /> : <Heart size={size} />
+  return (
+    <Button className={className} data-favorite={isFavorite}>
+      {label}
+    </Button>
+  )
+}
+
+const Button = styled.button``
