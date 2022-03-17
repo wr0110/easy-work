d6f77ac8b61ce992db20328849cd882e9cf7f7d9
wr0110 <wittyraccoon0110@gmail.com>
2022-03-17 11:44:18 +0500

    fix: fix format

--- src/features/favorite/model.ts
+++ src/features/favorite/model.ts
@@ -13,20 +13,15 @@ export const favoriteRemove = createEvent<{ favoriteID: string }>()
 
 export const $favoriteProjectsId = createStore<FavoritesProjects[]>([])
   .on(loadFavoritesProjectsFx.doneData, (_, projectsID) => projectsID)
-  .on(saveFavoriteProjectFx.doneData, (favorites, addedId) =>
-    favorites.concat(addedId)
-  )
+  .on(saveFavoriteProjectFx.doneData, (favorites, addedId) => favorites.concat(addedId))
 
 export const $favoriteIdx = $favoriteProjectsId.map((projects) =>
   projects.map(({ projectID }) => projectID)
 )
 
-export const $favoritesProjects = combine(
-  [$projects, $favoriteIdx],
-  ([projects, favoriteId]) => {
-    return projects.filter((project) => favoriteId.includes(project.projectID))
-  }
-)
+export const $favoritesProjects = combine([$projects, $favoriteIdx], ([projects, favoriteId]) => {
+  return projects.filter((project) => favoriteId.includes(project.projectID))
+})
 
 sample({
   source: favoriteAdd,
