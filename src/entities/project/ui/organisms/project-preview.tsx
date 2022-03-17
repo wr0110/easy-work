fb81c61a21c4c08247104cb53c8a81bf7e5ec65f
wr0110 <wittyraccoon0110@gmail.com>
2022-03-17 11:41:43 +0500

    feat: improve image and add card footer

--- src/entities/project/ui/organisms/project-preview.tsx
+++ src/entities/project/ui/organisms/project-preview.tsx
@@ -1,37 +1,49 @@
 import { Card, Text, Image, useTheme } from '@geist-ui/core'
 import { styled } from '@linaria/react'
 import { Link } from 'atomic-router-react'
-import React, { FC } from 'react'
+import React, { FC, ReactNode } from 'react'
 import type { Project } from '~/shared/api/requests'
 import { paths } from '~/shared/lib/paths'
 
 interface Props {
   project: Project
   className?: string
+  extra?: ReactNode
 }
 
-export const ProjectPreview: FC<Props> = ({ project, className }) => {
+export const ProjectPreview: FC<Props> = ({ project, className, extra }) => {
   const { type } = useTheme()
 
   return (
-    <Link className={className} to={paths.project(project.projectID)}>
-      <CardStyled data-theme={type} width="300px" height="340px" shadow>
-        <Image height="200px" width="300px" src={project.photoUrl} alt="project preview" />
-        <Text h4 mb={0}>
-          {project.title}
-        </Text>
-        <Text type="secondary" small>
-          {project.description}
-        </Text>
-      </CardStyled>
-    </Link>
+    <CardStyled data-theme={type} width="300px" shadow padding={0}>
+      <Link className={className} to={paths.project(project.projectID)}>
+        <Image width="300px" height="200px" src={project.photoUrl} alt="project preview" />
+        <GroupBody>
+          <Text h4 mb={0}>
+            {project.title}
+          </Text>
+          <Text type="secondary" small>
+            {project.description}
+          </Text>
+        </GroupBody>
+      </Link>
+      <Card.Footer>{extra}</Card.Footer>
+    </CardStyled>
   )
 }
 
 const CardStyled = styled(Card)`
+  & .content {
+    padding: 0 !important;
+  }
+
   &[data-theme='dark'] {
     &:hover {
       box-shadow: 0 0 0 1px #fff !important;
     }
   }
+`
+
+const GroupBody = styled.div`
+  padding: 0 1rem 1rem;
 `
