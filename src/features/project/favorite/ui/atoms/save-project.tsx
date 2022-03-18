19dd74e45d2e2beabeab8488a848df7eb39b9420
wr0110 <wittyraccoon0110@gmail.com>
2022-03-18 19:03:47 +0500

    feat: add remove and add button

--- src/features/project/favorite/ui/atoms/save-project.tsx
+++ src/features/project/favorite/ui/atoms/save-project.tsx
@@ -2,7 +2,7 @@ import { HeartFill, Heart } from '@geist-ui/icons'
 import { styled } from '@linaria/react'
 import { useStoreMap } from 'effector-react'
 import React, { FC } from 'react'
-import { $favoriteIdx } from '../../model'
+import { $favoriteIdx, favoriteAdd, favoriteRemove } from '../../model'
 
 interface Props {
   projectID: string
@@ -19,8 +19,24 @@ export const SaveProject: FC<Props> = ({ projectID, className, size }) => {
 
   const label = isFavorite ? <HeartFill size={size} color="red" /> : <Heart size={size} />
 
+  if (isFavorite) {
+    return (
+      <Button
+        className={className}
+        data-favorite={isFavorite}
+        onClick={() => favoriteRemove({ favoriteID: projectID })}
+      >
+        {label}
+      </Button>
+    )
+  }
+
   return (
-    <Button className={className} data-favorite={isFavorite}>
+    <Button
+      className={className}
+      data-favorite={isFavorite}
+      onClick={() => favoriteAdd({ favoriteID: projectID })}
+    >
       {label}
     </Button>
   )
