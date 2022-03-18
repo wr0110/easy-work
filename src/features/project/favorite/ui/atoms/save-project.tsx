bf7bf140cdb7ac097902d4d7852f819b6a80931b
wr0110 <wittyraccoon0110@gmail.com>
2022-03-18 19:01:43 +0500

    refactor: change to transfer project id

--- src/features/project/favorite/ui/atoms/save-project.tsx
+++ src/features/project/favorite/ui/atoms/save-project.tsx
@@ -1,15 +1,24 @@
 import { HeartFill, Heart } from '@geist-ui/icons'
 import { styled } from '@linaria/react'
+import { useStoreMap } from 'effector-react'
 import React, { FC } from 'react'
+import { $favoriteIdx } from '../../model'
 
 interface Props {
-  isFavorite: boolean
+  projectID: string
   className?: string
   size?: number
 }
 
-export const SaveProject: FC<Props> = ({ isFavorite, className, size }) => {
+export const SaveProject: FC<Props> = ({ projectID, className, size }) => {
+  const isFavorite = useStoreMap({
+    store: $favoriteIdx,
+    keys: [projectID],
+    fn: (favorites, [projectID]) => favorites.includes(projectID),
+  })
+
   const label = isFavorite ? <HeartFill size={size} color="red" /> : <Heart size={size} />
+
   return (
     <Button className={className} data-favorite={isFavorite}>
       {label}
