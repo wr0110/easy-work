5d66077d21870fb220789f9ccf8ad78fef23e94d
wr0110 <wittyraccoon0110@gmail.com>
2022-03-19 12:16:47 +0500

    feat: add check auth in settings page

--- src/pages/settings/profile/page.tsx
+++ src/pages/settings/profile/page.tsx
@@ -1,8 +1,16 @@
 import { Grid } from '@geist-ui/core'
 import { styled } from '@linaria/react'
 import React from 'react'
+import { checkAuthenticated, redirectSessionFailure } from '~/entities/session'
+import { userEditRoute } from '~/features/user/edit'
 import { UserEditForm } from '~/features/user/edit/ui'
 import { Sidebar } from '~/widgets/sidebar'
+
+checkAuthenticated({
+  when: userEditRoute.opened,
+  if: 'anonymous',
+  then: redirectSessionFailure,
+})
 
 export const Settings = () => {
   return (
