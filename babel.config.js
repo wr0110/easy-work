05c9f562fc70033890608441cbae52253ea4b56a
wr0110 <wittyraccoon0110@gmail.com>
2022-03-21 10:39:58 +0500

    fix: add ignore common js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
+    ['@babel/preset-env', { modules: false }],
     '@babel/preset-typescript',
     '@babel/preset-react',
     '@linaria',
