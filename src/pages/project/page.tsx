a0073d524f2ff8272e604e6baea04fd25842a52d
wr0110 <wittyraccoon0110@gmail.com>
2022-03-31 16:31:23 +0500

    refactor(pages): split page on components

--- src/pages/project/page.tsx
+++ src/pages/project/page.tsx
@@ -1,44 +1,59 @@
+import { variant } from '@effector/reflect'
+import { Loading } from '@geist-ui/core'
 import { styled } from '@linaria/react'
 import React from 'react'
 import { TaskPreview } from '~/entities/task/ui'
 import { BaseTemplate, GridBoards, PanelBoard } from '~/shared/ui'
+import { $pending } from './model'
 import { Header } from './ui/header'
 
-export const Project = () => {
-  return (
-    <>
+export const Project = variant({
+  source: $pending.map((pending) => (pending ? 'loading' : 'started')),
+  cases: {
+    loading: () => (
       <BaseTemplate header={<Header />}>
-        <GridBoards
-          idle={
-            <div>
-              <PanelBoardStyled heading="idle" />
-              <TaskPreview
-                title="HTTP is extensible"
-                description="Introduced in HTTP/1.0, HTTP headers make this protocol easy to extend and experiment with. New functionality can even be introduced by a simple agreement between a client and a server about a new header's semantics."
-              />
-            </div>
-          }
-          take={
-            <div>
-              <PanelBoardStyled heading="take" />
-              <TaskPreview
-                title="Hoverable card"
-                description="Introduced in HTTP/1.0, HTTP headers make this protocol easy to extend and experiment with. New functionality can even be introduced by a simple agreement between a client and a server about a new header's semantics."
-              />
-            </div>
-          }
-          resolve={
-            <div>
-              <PanelBoardStyled heading="resolve" />
-              <TaskPreview
-                title="Students"
-                description="Introduced in HTTP/1.0, HTTP headers make this protocol easy to extend and experiment with. New functionality can even be introduced by a simple agreement between a client and a server about a new header's semantics."
-              />
-            </div>
-          }
-        />
+        <Loading />
       </BaseTemplate>
-    </>
+    ),
+    started: () => (
+      <BaseTemplate header={<Header />}>
+        <Boards />
+      </BaseTemplate>
+    ),
+  },
+})
+
+export const Boards = () => {
+  return (
+    <GridBoards
+      idle={
+        <div>
+          <PanelBoardStyled heading="idle" />
+          <TaskPreview
+            title="HTTP is extensible"
+            description="Introduced in HTTP/1.0, HTTP headers make this protocol easy to extend and experiment with. New functionality can even be introduced by a simple agreement between a client and a server about a new header's semantics."
+          />
+        </div>
+      }
+      take={
+        <div>
+          <PanelBoardStyled heading="take" />
+          <TaskPreview
+            title="Hoverable card"
+            description="Introduced in HTTP/1.0, HTTP headers make this protocol easy to extend and experiment with. New functionality can even be introduced by a simple agreement between a client and a server about a new header's semantics."
+          />
+        </div>
+      }
+      resolve={
+        <div>
+          <PanelBoardStyled heading="resolve" />
+          <TaskPreview
+            title="Students"
+            description="Introduced in HTTP/1.0, HTTP headers make this protocol easy to extend and experiment with. New functionality can even be introduced by a simple agreement between a client and a server about a new header's semantics."
+          />
+        </div>
+      }
+    />
   )
 }
 
