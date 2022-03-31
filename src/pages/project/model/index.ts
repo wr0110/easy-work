86cf8b6b738e686a93c506e77d5a732e7616b380
wr0110 <wittyraccoon0110@gmail.com>
2022-03-31 16:32:59 +0500

    feat(pages): add load tasks on route opened

---
+++ src/pages/project/model/index.ts
@@ -0,0 +1,11 @@
+import { sample } from 'effector'
+import { pending } from 'patronum'
+import { loadTasksFx, loadTasksLifecycleFx } from '~/shared/api/requests'
+import { projectRoute } from '../route'
+
+export const $pending = pending({ effects: [loadTasksLifecycleFx] })
+
+sample({
+  clock: projectRoute.opened,
+  target: [loadTasksLifecycleFx, loadTasksFx],
+})
