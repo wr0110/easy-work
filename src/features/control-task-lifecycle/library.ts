ee12c7620eec09ed4899be7346236c2c329ff2e1
wr0110 <wittyraccoon0110@gmail.com>
2022-04-23 22:42:54 +0500

    feat: add check status in drag ended

--- src/features/control-task-lifecycle/library.ts
+++ src/features/control-task-lifecycle/library.ts
@@ -1,6 +1,6 @@
 import type { DragEndEvent, DragOverEvent, DragStartEvent } from '@dnd-kit/core'
 import { attach, createEvent, createStore, sample } from 'effector'
-import { splitMap } from 'patronum'
+import { debug, splitMap } from 'patronum'
 import type { Status, TaskLifecycle } from '~/shared/api/requests'
 import { isDefined } from '~/shared/lib/type-guard/index'
 
@@ -12,7 +12,12 @@ export const changeTaskStatus = (
   return tasks.map((task) => (task.taskId === id ? { ...task, status } : task))
 }
 
-export type TaskMoveitem = Pick<TaskLifecycle, 'taskId' | 'status'> & { target: Status }
+export const notPositionRepeat = (activeId: string, overId: string) => activeId !== overId
+
+export const findBoardStatus = (tasks: TaskLifecycle[], id: string) => {
+  return tasks.find((task) => task.taskId === id)?.status
+}
+
 export type ID = Pick<TaskLifecycle, 'taskId'>
 
 export const createTaskLifeCycleState = () => {
@@ -24,8 +29,6 @@ export const createTaskLifeCycleState = () => {
     .on(initItems, (_, tasks) => tasks)
     .on(addItems, (prevTasks, tasks) => prevTasks.concat(tasks))
     .on(removeItem, (tasks, { taskId }) => tasks.filter((task) => task.taskId !== taskId))
-
-  const taskMoved = createEvent<TaskMoveitem>()
 
   const takeTaskFx = attach({
     source: $lifecycle,
@@ -41,36 +44,50 @@ export const createTaskLifeCycleState = () => {
     },
   })
 
-  interface MoveTaskOver {
-    fromRaised: string
+  interface MoveTask {
+    fromRaisedId: string
     overElement: string
   }
 
-  const moveTaskOverFx = attach({
+  const taskMoveFx = attach({
     source: $lifecycle,
-    effect: (lifecycle, { fromRaised, overElement }: MoveTaskOver) => {
-      const targetElement = lifecycle.find((task) => task.taskId === overElement)
+    effect: (lifecycle, { fromRaisedId, overElement }: MoveTask) => {
+      const isTask = lifecycle.some((task) => task.taskId === overElement)
 
-      if (!targetElement) {
-        const board = overElement as Status
-        return changeTaskStatus(lifecycle, fromRaised, board)
+      const targetBoard = isTask ? findBoardStatus(lifecycle, overElement) : overElement
+
+      if (!targetBoard) throw new Error(`${targetBoard} not provided`)
+
+      return changeTaskStatus(lifecycle, fromRaisedId, targetBoard as Status)
+    },
+  })
+
+  const taskDragEndedFx = attach({
+    source: $lifecycle,
+    effect: (lifecycle, { fromRaisedId, overElement }: MoveTask) => {
+      const fromRaisedTask = lifecycle.find((task) => task.taskId === fromRaisedId)
+      const isTask = lifecycle.some((task) => task.taskId === overElement)
+
+      if (!fromRaisedTask) throw new Error(`${fromRaisedId} not provided in ${lifecycle}`)
+
+      return {
+        fromRaisedTask,
+        targetBoard: isTask ? findBoardStatus(lifecycle, overElement) : overElement,
       }
-
-      return changeTaskStatus(lifecycle, fromRaised, targetElement.status)
     },
   })
 
   const { take, resolve } = splitMap({
-    source: taskMoved,
+    source: taskDragEndedFx.doneData,
     cases: {
-      take: (moveitem) => {
-        if (moveitem.status === 'idle' && moveitem.target === 'take') {
-          return { taskId: moveitem.taskId } as ID
+      take: ({ fromRaisedTask, targetBoard }) => {
+        if (fromRaisedTask.status === 'idle' && targetBoard === 'take') {
+          return { taskId: fromRaisedTask.taskId } as ID
         }
       },
-      resolve: (moveitem) => {
-        if (moveitem.status === 'take' && moveitem.target === 'resolve') {
-          return { taskId: moveitem.taskId } as ID
+      resolve: ({ fromRaisedTask, targetBoard }) => {
+        if (fromRaisedTask.status === 'take' && targetBoard === 'resolve') {
+          return { taskId: fromRaisedTask.taskId } as ID
         }
       },
     },
@@ -87,7 +104,7 @@ export const createTaskLifeCycleState = () => {
   })
 
   sample({
-    clock: [takeTaskFx.doneData, resolveTaskFx.doneData, moveTaskOverFx.doneData],
+    clock: [takeTaskFx.doneData, resolveTaskFx.doneData, taskMoveFx.doneData],
     target: $lifecycle,
   })
 
@@ -104,27 +121,24 @@ export const createTaskLifeCycleState = () => {
   })
 
   sample({
-    clock: dragOver,
-    filter: ({ active, over }) => isDefined(over) && over.id !== active.id,
+    clock: dragEnded,
+    filter: ({ active, over }) => isDefined(over) && notPositionRepeat(over.id, active.id),
     fn: ({ active, over }) => ({
-      fromRaised: active.id,
-      // @fix type guard
+      fromRaisedId: active.id,
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       overElement: over!.id,
     }),
-    target: moveTaskOverFx,
+    target: taskDragEndedFx,
   })
 
   return {
     addItems,
     initItems,
-    taskMoved,
     dragEnded,
     dragOver,
     $lifecycle,
     removeItem,
     dragStarted,
     $activeItemId,
-    moveTaskOverFx,
   }
 }
