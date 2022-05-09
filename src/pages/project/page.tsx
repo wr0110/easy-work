af471fc3b0755c414faebd0e89b0c729bbb257d6
wr0110 <wittyraccoon0110@gmail.com>
2022-05-09 22:30:21 +0500

    feat(pages): show modal for create task

--- src/pages/project/page.tsx
+++ src/pages/project/page.tsx
@@ -1,6 +1,9 @@
 import { variant } from '@effector/reflect'
-import { Grid, Loading } from '@geist-ui/core'
+import { Grid, Loading, Modal, Spacer } from '@geist-ui/core'
+import { useStore } from 'effector-react'
 import React from 'react'
+import { $isOpenForm, hideTaskForm, taskSave } from '~/entities/task'
+import { Create, DescriptionField, TitleField } from '~/entities/task/ui'
 import { BoardsBaseStructs } from '~/features/task/control-task-lifecycle/ui'
 import { BaseTemplate } from '~/shared/ui'
 import { $pending } from './model'
@@ -22,11 +25,29 @@ export const Project = () => {
   )
 }
 
+export const CreateTaskModal = () => {
+  const isOpen = useStore($isOpenForm)
+  return (
+    <Modal visible={isOpen} onClose={hideTaskForm}>
+      <Modal.Title>Create task</Modal.Title>
+      <Modal.Content>
+        <TitleField />
+        <Spacer h={0.6} />
+        <DescriptionField />
+      </Modal.Content>
+      <Modal.Action onClick={() => hideTaskForm()} passive>
+        Cancel
+      </Modal.Action>
+      <Modal.Action onClick={() => taskSave()}>Submit</Modal.Action>
+    </Modal>
+  )
+}
+
 export const Boards = () => {
   return (
     <Grid.Container gap={10} justify="center" height="100px">
-      <BoardsBaseStructs />
       <BoardsBaseStructs extra={<Create />} />
+      <CreateTaskModal />
     </Grid.Container>
   )
 }
