8b8f2883d0f8726f55c18312e56d92da7016894b
wr0110 <wittyraccoon0110@gmail.com>
2022-05-09 18:08:45 +0500

    refactor: change structure

--- src/pages/project/page.tsx
+++ src/pages/project/page.tsx
@@ -1,7 +1,7 @@
 import { variant } from '@effector/reflect'
 import { Grid, Loading } from '@geist-ui/core'
 import React from 'react'
-import { BoardsBaseStructs } from '~/features/control-task-lifecycle/ui'
+import { BoardsBaseStructs } from '~/features/task/control-task-lifecycle/ui'
 import { BaseTemplate } from '~/shared/ui'
 import { $pending } from './model'
 import { Header } from './ui/header'
