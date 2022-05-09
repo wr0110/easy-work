83d7eb77d7aa5a91b4c4a2483e5d03b3b7629461
wr0110 <wittyraccoon0110@gmail.com>
2022-05-09 22:32:24 +0500

    feat(pages): send new task to boards db

--- src/pages/project/model/index.ts
+++ src/pages/project/model/index.ts
@@ -1,6 +1,12 @@
 import { sample } from 'effector'
 import { pending } from 'patronum'
-import { loadTasksFx, loadTasksLifecycleFx } from '~/shared/api/requests'
+import {
+  addTaskFx,
+  addTaskToLifecycleFx,
+  loadTasksFx,
+  loadTasksLifecycleFx,
+  Status,
+} from '~/shared/api/requests'
 import { projectRoute } from '../route'
 
 export const $pending = pending({ effects: [loadTasksLifecycleFx] })
@@ -10,4 +16,12 @@ sample({
   filter: projectRoute.$isOpened,
   fn: (route) => ({ projectID: route.params.id }),
   target: [loadTasksLifecycleFx, loadTasksFx],
+})
+
+sample({
+  clock: addTaskFx.doneData,
+  source: projectRoute.$params,
+  filter: Boolean,
+  fn: (params, taskId) => ({ projectID: params.id, status: 'idle' as Status, taskId }),
+  target: addTaskToLifecycleFx,
 })
