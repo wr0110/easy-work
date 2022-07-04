7d5a01cbc2fed277d149ad0138a08112f6a992bf
wr0110 <wittyraccoon0110@gmail.com>
2022-07-04 15:18:07 +0500

    fix(feature/favorites): fix fill on dark theme

--- src/features/project/favorite/ui/atoms/favorite-add.tsx
+++ src/features/project/favorite/ui/atoms/favorite-add.tsx
@@ -1,15 +1,19 @@
 import { Button } from '@geist-ui/core'
 import { Star } from '@geist-ui/icons'
-import { useStoreMap } from 'effector-react'
+import { useStore, useStoreMap } from 'effector-react'
 import React, { FC } from 'react'
+import { $isDark } from '~/features/theme'
 import { $favoritesIds, favoriteAdd, favoriteRemove } from '../../model'
 
 export const FavoriteAdd: FC<{ projectID: string }> = ({ projectID }) => {
+  const isDark = useStore($isDark)
   const isFavorite = useStoreMap({
     store: $favoritesIds,
     keys: [projectID],
     fn: (favorites, [projectID]) => favorites.includes(projectID),
   })
+
+  const fill = isDark ? 'white' : 'black'
 
   if (isFavorite) {
     return (
@@ -17,7 +21,7 @@ export const FavoriteAdd: FC<{ projectID: string }> = ({ projectID }) => {
         auto
         type="abort"
         onClick={() => favoriteRemove({ projectId: projectID })}
-        iconRight={<Star fill="black" />}
+        iconRight={<Star fill={fill} />}
         scale={2 / 3}
       />
     )
@@ -28,7 +32,7 @@ export const FavoriteAdd: FC<{ projectID: string }> = ({ projectID }) => {
       auto
       type="abort"
       onClick={() => favoriteAdd({ projectId: projectID })}
-      iconRight={<Star />}
+      iconRight={<Star fill={fill} />}
       scale={2 / 3}
     />
   )
