fc182a0f62a1ebc6af9c6fa07bef09f78b5b4321
wr0110 <wittyraccoon0110@gmail.com>
2022-07-29 12:58:42 +0500

    build: add tree shake to icons

--- babel.config.js
+++ babel.config.js
@@ -1,33 +1,69 @@
-module.exports = {
-  presets: [
+module.exports = function (api) {
+  const isProduction = api.env('production')
+  const isTests = api.env('test')
+
+  const presets = [
     ['@babel/preset-env', { modules: false }],
     '@babel/preset-typescript',
     ['@babel/preset-react', { runtime: 'automatic' }],
     '@linaria',
-  ],
-  plugins: [
+  ]
+
+  const plugins = [
     [
       'effector/babel-plugin',
       {
         factories: ['src/shared/api/requests/request', '~/shared/api/requests/request'],
       },
     ],
-  ],
-  env: {
-    test: {
-      plugins: [
-        'effector/babel-plugin',
-        '@babel/plugin-transform-runtime',
-        '@babel/plugin-transform-modules-commonjs',
-        [
-          'module-resolver',
-          {
-            alias: {
-              '~': './src',
-            },
+  ]
+
+  if (isProduction) {
+    plugins.push([
+      'import',
+      {
+        libraryName: '@geist-ui/icons',
+        libraryDirectory: '',
+        customName: (name) => {
+          const pathByLib = (name) => `@geist-ui/icons/${name}`
+          const parseToCamelCase = (word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`
+
+          const separator = '-'
+          const hasDash = name.includes(separator)
+
+          if (hasDash) {
+            const words = name.split('-')
+            const iconsWithDashed = words.slice(1).map(parseToCamelCase)
+
+            const finallyIconName = words[0] + iconsWithDashed.join('')
+
+            return pathByLib(finallyIconName)
+          }
+
+          return pathByLib(name)
+        },
+      },
+    ])
+  }
+
+  if (isTests) {
+    plugins.push([
+      'effector/babel-plugin',
+      '@babel/plugin-transform-runtime',
+      '@babel/plugin-transform-modules-commonjs',
+      [
+        'module-resolver',
+        {
+          alias: {
+            '~': './src',
           },
-        ],
+        },
       ],
-    },
-  },
+    ])
+  }
+
+  return {
+    presets,
+    plugins: plugins.filter(Boolean),
+  }
 }
